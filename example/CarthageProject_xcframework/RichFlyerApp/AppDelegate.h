eaaf1376b1adf42ceae101c80cddf70fdb1b4822
Takeshi Goto <t-goto@infocity.co.jp>
2021-07-16 13:18:53 +0900

    update example for RichFlyer SDK v1.2.8 add example using xcframework

---
+++ example/CarthageProject_xcframework/RichFlyerApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RichFlyer
+//
+//  Copyright © 2019年 INFOCITY,Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <RichFlyer/RichFlyer.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RFNotificationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ example/CarthageProject_xcframework/RichFlyerApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RichFlyer
+//
+//  Copyright © 2019年 INFOCITY,Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
